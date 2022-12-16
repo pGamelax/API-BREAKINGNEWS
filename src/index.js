e049ef11ef9c5354d2f86dcd5e66faecd1a0939a
Pedro Gamela <pedrogamela2003@gmail.com>
2022-12-16 03:10:16 -0300

    src

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import express from 'express';
-import connectDatabase from './src/database/database.js';
+import connectDatabase from './database/database.js';
 import dotenv from "dotenv"
 
 
-import userRoute from './src/routes/user.route.js';
-import authRoute from './src/routes/auth.route.js';
-import newsRoute from './src/routes/news.route.js'
-import swaggerRoute from './src/routes/swagger.route.js'
+import userRoute from './routes/user.route.js';
+import authRoute from './routes/auth.route.js';
+import newsRoute from './routes/news.route.js'
+import swaggerRoute from './routes/swagger.route.js'
 
 dotenv.config()
 
